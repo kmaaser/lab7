397b265ee762ead980a93c9ae857593cdf899776
kmaaser <kmaaser@capital.edu>
2017-04-05 18:34:20 -0400

    spacing

--- Lab 7/Lab 7/main.cpp
+++ Lab 7/Lab 7/main.cpp
@@ -9,17 +9,22 @@ int main()
 	double storedvalues;
 	int number = 0, i;
 	int value;
-	
+
+	//asks user for how many values they want to enter
 	cout << "Enter number of values: " << endl;
 	cin >> value;
 
+	//array is size 10000 because we can assume
+	//no more than 10000 will be entered
 	int myarray[10000];
+
+	//making sure the numbers are added to myarray
 	for (int i = 0; i < value; ++i) {
 		cout << "Enter number " << number + 1 << " : " << endl;
 		cin >> storedvalues;
 		myarray[i] = storedvalues;
 	}
-
+	//prints out the mean, median, standard deviation, min, max
 	cout << mean << endl;
 	cout << median << endl;
 	cout << standardDeviation << endl;
