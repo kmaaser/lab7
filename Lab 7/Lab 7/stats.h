5dcba7098f504ba423231afa54956140d931d9ec
Chris Cannon <Chris.c.cannon@gmail.com>
2017-04-05 23:21:42 -0400

    Update header back to normal

--- Lab 7/Lab 7/stats.h
+++ Lab 7/Lab 7/stats.h
@@ -4,33 +4,22 @@
 #ifndef __STATS__H
 #define __STATS__H
 
-class stats{
+
 //declarations of the methods
 
-//private helper methods
-private:
-  void sort(double nums[], int n);
-  bool lessDouble(double v, double w);
-  void exch(double nums[], int i, int j);
+//returns average of numbers
+double mean(double nums[], int n);
 
-//public methods
-public:
+//returns median of numbers
+double median(double nums[], int n);
 
-  //returns average of numbers
-  double mean(double nums[], int n);
+//returns standard deviation of numbers
+double standardDeviation(double nums[], int n);
 
-  //returns median of numbers
-  double median(double nums[], int n);
-
-  //returns standard deviation of numbers
-  double standardDeviation(double nums[], int n);
-
-
-  //returns minimum of numbers
-  double min(double nums[], int n);
-
+//returns minimum of numbers
+double min(double nums[], int n);
 
 // returns maximum of numbers
 double max(double nums[], int n);
-};
+
 #endif
